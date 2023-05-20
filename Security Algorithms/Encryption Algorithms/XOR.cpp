0c5333cc950dbe7c559cd2162349aa60746de9ce
Philip <Philipjames2004@gmail.com>
2023-05-20 17:26:32 -0400

    Added a more secure xor program to better deal with an frequency analysis by constantly changing the given key

--- Security Algorithms/Encryption Algorithms/XOR.cpp
+++ Security Algorithms/Encryption Algorithms/XOR.cpp
@@ -17,7 +17,14 @@
     into your plaintext file. The output will be in a file named 'ciphertext.txt'. The key here is
     similar to a password. The longer and stranger it is, the better.
 
+    Updates:
+    Added a more secure encryption and decrytion function. Rather than using a constant key provided
+    by the user, this will change the given key after every xor operation. This should make the
+    cipher text MUCH less susceptible to a frequency analysis or some brute force attack.
+
     TODO:
+    Change output so it more uniform between different modes/cases
+
 */
 
 void xorDecrypt(std::string ciphertextFileName, std::string key) {
@@ -53,6 +60,79 @@ void xorDecrypt(std::string ciphertextFileName, std::string key) {
     cipherFile.close();
 
     std::cout << "Data have been saved to 'plaintext.txt'" << std::endl;
+}
+
+void advancedXorDecryption(std::string ciphertextFileName, std::string key) {
+    //open encrypted file for reading
+    std::ifstream cipherFile;
+    cipherFile.open(ciphertextFileName);
+
+    if (cipherFile.fail()) {
+        //file could not be opened for reading
+        std::cout << "Ciphertext file could not be opened\n";
+        return;
+    }
+    
+    std::ofstream plainFile;
+    plainFile.open("plaintext.txt");
+    if (plainFile.fail()) {
+        //File could not be opened for writing
+        std::cout << "Plaintext file could not be opened\n";
+        return;
+    }
+
+    std::cout << "Decrypting data... " << std::endl;
+    std::string buff = "";
+    for (int i = 0; cipherFile >> buff; i++) {
+        char c = key[i % key.length()];
+        //stoi() converts the string to an ascii integer
+        c ^= stoi(buff);
+        plainFile << c;
+        key[i % key.length()] = ((key[i % key.length()] + 1) % 127) + 1;
+    }
+
+    //close files
+    plainFile.close();
+    cipherFile.close();
+
+    std::cout << "Data have been saved to 'plaintext.txt'" << std::endl;
+}
+
+void advancedXorEncryption(std::string plaintextFileName, std::string key) {
+    //open plaintext file
+    std::ifstream plaintextFile;
+    plaintextFile.open(plaintextFileName);
+
+    if (!plaintextFile.is_open()) {
+        std::cout << "Plaintext file could not be opened\n";
+        return;
+    }
+
+    //Open cipher text file
+    std::ofstream ciphertextFile;
+    ciphertextFile.open("ciphertext.txt");
+    
+    if (!ciphertextFile.is_open()) {
+        std::cout << "Error writing data\n";
+    }
+
+    std::cout << "Encrypting ..." << std::endl;
+    char c;
+
+    //Xors each character in plaintext with a character in key
+    for (int i = 0; plaintextFile.get(c); i++) {
+        ciphertextFile << (c ^ key.at(i % key.length())) << " ";
+        int x = ((key[i % key.length()] + 1) % 127) + 1;
+        key[i % key.length()] = ((key[i % key.length()] + 1) % 127) + 1;
+        std::cout << key << " - > " << x << std::endl;
+    }
+
+    //close files
+    ciphertextFile.close();
+    plaintextFile.close();
+
+    std::cout << "Data have been saved to 'ciphertext.txt'" << std::endl;
+
 }
 
 void xorEncrypt(std::string plaintextFileName, std::string key) {
@@ -96,33 +176,52 @@ int main() {
     bool encrypt = false;
     std::cout << "XOR encryptor\n";
     while (1) {
-        std::cout << "Encrypt or Decrypt (1 or 2): ";
+        std::cout << "Encrypt (1)\nDecrypt(2)\nAdvanced Encryption (3)\nAdvanced Decryption (4)\n";
         int optionTemp = 0;
         std::cin >> optionTemp;
-        encrypt = (optionTemp == 1) ? true : false;
-        if (!(optionTemp == 1 || optionTemp == 2)) {
-            std::cout << "Invalid Option\n";
-        } else {
+        if (optionTemp == 1) {
+            std::string plaintextFileName;
+            std::string key;
+            std::cout << "Please enter plaintext file name: ";
+            std::cin >> plaintextFileName;
+            std::cout << "Please enter a key: ";
+            std::cin >> key;
+            xorEncrypt(plaintextFileName, key);
             break;
         }
-    }
-
-    if (encrypt) {
-        std::string plaintextFileName;
-        std::string key;
-        std::cout << "Please enter plaintext file name: ";
-        std::cin >> plaintextFileName;
-        std::cout << "Please enter a key: ";
-        std::cin >> key;
-        xorEncrypt(plaintextFileName, key);
-    } else {
-        std::string ciphertextFileName;
-        std::string key;
-        std::cout << "Please enter ciphertext file name: ";
-        std::cin >> ciphertextFileName;
-        std::cout << "Please enter your key: ";
-        std::cin >> key;
-        xorDecrypt(ciphertextFileName, key);
+        if (optionTemp == 2) {
+            std::string ciphertextFileName;
+            std::string key;
+            std::cout << "Please enter ciphertext file name: ";
+            std::cin >> ciphertextFileName;
+            std::cout << "Please enter your key: ";
+            std::cin >> key;
+            xorDecrypt(ciphertextFileName, key);
+            break;
+        }
+        if (optionTemp == 3) {
+            std::string plaintextFileName;
+            std::string key;
+            std::cout << "Please enter plaintext file name: ";
+            std::cin >> plaintextFileName;
+            std::cout << "Please enter a key: ";
+            std::cin >> key;
+            advancedXorEncryption(plaintextFileName, key);
+            break;
+        }
+        if (optionTemp == 4) {
+            std::string ciphertextFileName;
+            std::string key;
+            std::cout << "Please enter ciphertext file name: ";
+            std::cin >> ciphertextFileName;
+            std::cout << "Please enter your key: ";
+            std::cin >> key;
+            advancedXorDecryption(ciphertextFileName, key);
+            break;
+        }
+        else {
+            std::cout << "Invalid selection\n";
+        }
     }
     return 0;
 }
