a05ca48de955b33423d1af50601d1cdc52f20b03
Philip <Philipjames2004@gmail.com>
2023-05-27 09:24:57 -0500

    more work with sha

--- Security Algorithms/Encryption Algorithms/sha.cpp
+++ Security Algorithms/Encryption Algorithms/sha.cpp
@@ -2,7 +2,8 @@
 
 int main() {
     //read in a block of data stored in a char array
-
+    int a = 0x6a09e667;
+    std::cout << a;
     //do any padding (for the last chuck)
 
     //convert chars to numbers if needed
