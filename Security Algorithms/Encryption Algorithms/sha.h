72efe9b2d78fe876af15581a208731915576f180
Philip <Philipjames2004@gmail.com>
2023-05-27 11:15:12 -0500

    Added a basic h file

---
+++ Security Algorithms/Encryption Algorithms/sha.h
@@ -0,0 +1,18 @@
+#ifndef sha_H
+#define sha_H
+
+extern int a;
+//Inital hash values
+extern unsigned int h0;
+extern unsigned int h1;
+extern unsigned int h2;
+extern unsigned int h3;
+extern unsigned int h4;
+extern unsigned int h5;
+extern unsigned int h6;
+extern unsigned int h7;
+
+//Initialize array of round constants:
+extern unsigned int k[64];
+
+#endif
