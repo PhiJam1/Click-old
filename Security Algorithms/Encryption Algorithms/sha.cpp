6e2d4a3077933c66799608442113001feef2c1a9
Philip James <philipjames2004@gmail.com>
2023-06-02 15:55:19 -0500

    Works great as a RNG

--- Security Algorithms/Encryption Algorithms/sha.cpp
+++ Security Algorithms/Encryption Algorithms/sha.cpp
@@ -7,8 +7,6 @@
 
 #include "sha.h"
 
-int a = 9;
-
 //Inital hash values
 unsigned int h0 = 0x6a09e66;
 unsigned int h1 = 0xbb67ae85;
@@ -19,18 +17,6 @@ unsigned int h5 = 0x9b05688c;
 unsigned int h6 = 0x1f83d9ab;
 unsigned int h7 = 0x5be0cd19;
 
-
-//Initialize array of round constants:
-unsigned int k[] = { 0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 
-            0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 
-            0x9bdc06a7, 0xc19bf174,0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 
-            0x4a7484aa, 0x5cb0a9dc, 0x76f988da,0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
-            0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,0x27b70a85, 0x2e1b2138, 0x4d2c6dfc,
-            0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b,
-            0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,0x19a4c116, 
-            0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-            0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7,
-            0xc67178f2};
 
 
 // This only works for one block rn (that is 55 characters)
@@ -143,6 +129,47 @@ void createSchedule (int *messageBlock, char *block) {
     }
 }
 
+void shaTransform(int * msgSch) {
+    unsigned int a = h0;
+    unsigned int b = h1;
+    unsigned int c = h2;
+    unsigned int d = h0;
+    unsigned int e = h4;
+    unsigned int f = h5;
+    unsigned int g = h6;
+    unsigned int h = h7;
+
+    for (int i = 0; i < 64; i++) {
+        unsigned int s1 = ROTRIGHT(e, 6) ^ ROTRIGHT(e, 11) ^ ROTRIGHT(e, 25);
+        unsigned int ch = (e & f) ^ ((~e) & g);
+        unsigned int temp1 = h + s1 + ch + k[i] + msgSch[i];
+        unsigned int s0 = ROTRIGHT(a, 2) ^ ROTRIGHT(a, 13) ^ ROTRIGHT(a, 22);
+        unsigned int maj = (a & b) ^ (a & c) ^ (b & c);
+        unsigned int temp2 = s0 + maj;
+        h = g;
+        g = f;
+        f = e;
+        e = d + temp1;
+        d = c;
+        c = b;
+        b = a;
+        a = temp1 + temp2;
+
+        h0 = h0 + a;
+        h1 = h1 + b;
+        h2 = h2 + c;
+        h3 = h3 + d;
+        h4 = h4 + e;
+        h5 = h5 + f;
+        h6 = h6 + g;
+        h7 = h7 + h;
+
+
+    }
+    std::cout << h0 << h1 << h2 << h3 << h4 << h5 << h6 << h7 << std::endl;
+
+}
+
 
 int main() {
     char block[64]; //64 chars = 512 bits
@@ -160,6 +187,9 @@ int main() {
 
     int messageSch[64];
     createSchedule(messageSch, block);
+    shaTransform(messageSch);
+
+    //so there three function would be called until we run out of data. then print out the inital h values
 
     return 0;
 }
