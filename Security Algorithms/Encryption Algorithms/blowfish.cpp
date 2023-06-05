3ec13e4d6c6ec67005e85f86fa0c4c29b06338a2
Philip <Philipjames2004@gmail.com>
2023-06-05 08:33:25 -0500

    bf work

--- Security Algorithms/Encryption Algorithms/blowfish.cpp
+++ Security Algorithms/Encryption Algorithms/blowfish.cpp
@@ -3,8 +3,21 @@
 #include <fstream>
 #include <bits/stdc++.h>
 
-void f(std::string plaintext) {
+//This will take in a user's key and append '!'s until
+//it's size is a multiple of 32 bits.
+void getKey() {
+
+}
+
+/*
+    This function will read in data in 64 bit
+    chuncks and return true while there is more
+    data to be read. When padding is needed, it will
+    set the number of padded values to the padding variable.
+*/
+bool getPlaintext(std::ifstream& inFile, int *padding) {
     
+    return false;
 }
 
 void generateSubKeys(int *pBoxes) {
@@ -15,6 +28,9 @@ void encrypt(std::string plaintext) {
 
 }
 
+void f(std::string plaintext) {
+    
+}
 
 int main() {
 
